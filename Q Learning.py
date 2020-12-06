22a5df9d37d41203be4a6ed2f46bff5e789be4bd
Mimo <gunjan.mtbpaul@gmail.com>
2020-12-06 10:47:05 -0800

    new

--- Q Learning.py
+++ Q Learning.py
@@ -3,45 +3,33 @@ import numpy as np
 
 env = gym.make("MountainCar-v0")
 
-
-# print(env.observation_space.high)
-# print(env.observation_space.low)
-# print(env.action_space.n)
-
-DISCRETE_OS_SIZE = [20]*len(env.observation_space.high)
+DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high -
-                        env.observation_space.low)/DISCRETE_OS_SIZE
-# print(discrete_os_win_size)
+                        env.observation_space.low) / DISCRETE_OS_SIZE
 
-q_table = np.random.uniform(
-    low=-2, high=0, size=(DISCRETE_OS_SIZE+[env.action_space.n]))
-
-# print(q_table.shape)
-# print(q_table)
+q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
+ep_rewards = []
+aggr_ep_rewards = {"ep": [], "avg": [], "min": [], "max": []}
 
 # HYPER Parameters
 LEARNING_RATE = 0.1
 DISCOUNT = 0.95
-EPISODES = 25000
-SHOW_EVERY = 2000
+EPISODES = 2000
+SHOW_EVERY = 500
 
 epsilon = 0.5
 START_EPSILON_DECAYING = 1
-END_EPSILON_DECAYING = EPISODES//2
-epsilon_decay_value = epsilon/(END_EPSILON_DECAYING- START_EPSILON_DECAYING)
-
-
-
-
-
+END_EPSILON_DECAYING = EPISODES // 2
+epsilon_decay_value = epsilon / (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
 
 def get_discrete_state(state):
-    discrete_state = (state-env.observation_space.low) / discrete_os_win_size
+    discrete_state = (state - env.observation_space.low) / discrete_os_win_size
     return tuple(discrete_state.astype(np.int))
 
 
 for episode in range(EPISODES):
+    episode_reward = 0
     if episode % SHOW_EVERY == 0:
         print(episode)
         render = True
@@ -50,30 +38,41 @@ for episode in range(EPISODES):
     discrete_state = get_discrete_state(env.reset())
     done = False
     while not done:
-        if np.random.random()>epsilon:
+        if np.random.random() > epsilon:
             action = np.argmax(q_table[discrete_state])
         else:
             action = np.random.randint(0, env.action_space.n)
-            
+
         new_State, reward, done, _ = env.step(action)
+        episode_reward += reward
+
         new_discrete_state = get_discrete_state(new_State)
         if render:
             env.render()
+
         if not done:
             max_future_q = np.max(q_table[new_discrete_state])
-            current_q = q_table[discrete_state+(action,)]
+            current_q = q_table[discrete_state + (action,)]
 
-            new_q = (1 - LEARNING_RATE)*current_q + \
-                LEARNING_RATE*(reward+DISCOUNT*max_future_q)
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
 
-            q_table[discrete_state+(action,)] = new_q
+            q_table[discrete_state + (action,)] = new_q
 
         elif new_State[0] >= env.goal_position:
             print(f"success on episode: {episode}")
-            q_table[discrete_state+(action,)] = 0
+            q_table[discrete_state + (action,)] = 0
 
         discrete_state = new_discrete_state
-    if END_EPSILON_DECAYING >= epsilon>= START_EPSILON_DECAYING:
+    if END_EPSILON_DECAYING >= epsilon >= START_EPSILON_DECAYING:
         epsilon -= epsilon_decay_value
+    ep_rewards.append(episode_reward)
+
+    if not episode % SHOW_EVERY:
+        average_reward = sum(ep_rewards[-SHOW_EVERY:]) / len(ep_rewards[-SHOW_EVERY])
+        aggr_ep_rewards["ep"].append(episode)
+        aggr_ep_rewards["avg"].append(average_reward)
+        aggr_ep_rewards["min"].append(min(ep_rewards[-SHOW_EVERY:]))
+        aggr_ep_rewards["max"].append(max(ep_rewards[-SHOW_EVERY:]))
+        a
 
 env.close()
