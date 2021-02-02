4a001cb21e220b36a2d5b4c404aebb5001b175e5
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2021-02-03 00:31:52 +0530

    new

--- Deep Q Learning/dqn.py
+++ Deep Q Learning/dqn.py
@@ -1,3 +1,6 @@
-class DqnAgent:
-    def create_model(self):
-        model = Sequential
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
+from keras.callback import Tensorboard
+# class DqnAgent:
+#     def create_model(self):
+#         model = Sequential()
