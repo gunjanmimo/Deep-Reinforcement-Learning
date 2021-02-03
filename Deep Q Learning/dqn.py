9f5a7d21b6652628df05ddd83a50c6ddcaa9c2d9
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2021-02-03 12:00:21 +0530

    new

--- Deep Q Learning/dqn.py
+++ Deep Q Learning/dqn.py
@@ -1,12 +1,73 @@
 import keras
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
 import keras.callbacks
 from keras.callbacks import TensorBoard
 from keras.optimizers import Adam
 from collections import deque
+import time
+import numpy as np
 
 REPLAY_MEMORY_SIZE = 50000
+MODEl_NAME = "256x2"
+
+
+# Own Tensorboard class
+class ModifiedTensorBoard(TensorBoard):
+
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.model = None
+        self.TB_graph = tf.compat.v1.Graph()
+        with self.TB_graph.as_default():
+            self.writer = tf.summary.create_file_writer(self.log_dir,
+                                                        flush_millis=5000)
+            self.writer.set_as_default()
+            self.all_summary_ops = tf.compat.v1.summary.all_v2_summary_ops()
+        self.TB_sess = tf.compat.v1.InteractiveSession(graph=self.TB_graph)
+        self.TB_sess.run(self.writer.init())
+
+    # Overriding this method to stop creating default log writer
+    def set_model(self, model):
+        self.model = model
+        self._train_dir = self.log_dir + '\\train'
+
+    # Override, saves logs with our step number
+    # (otherwise every .fit() will start writing from 0th step)
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    # Override
+    # We train for one batch only, no need to save anything at epoch end
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    def on_train_begin(self, logs=None):
+        pass
+
+    # Override, so won't close writer
+    def on_train_end(self, _):
+        pass
+
+    # added for performance?
+    def on_train_batch_end(self, _, __):
+        pass
+
+    # Custom method for saving own metrics
+    # Creates writer, writes custom metrics and closes writer
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
+
+    def _write_logs(self, logs, index):
+        for name, value in logs.items():
+            self.TB_sess.run(self.all_summary_ops)
+            if self.model is not None:
+                name = f'{name}_{self.model.name}'
+            self.TB_sess.run(tf.summary.scalar(name, value, step=index))
+        self.model = None
 
 
 class DqnAgent:
@@ -18,6 +79,9 @@ class DqnAgent:
         self.target_model.set_weights(self.model.get_weights())
 
         self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+        self.tensorboard = ModifiedTensorBoard(
+            log_dir=f"logs/{MODEl_NAME}-{int(time.time())}")
+        self.target_update_counter = 0
 
     def create_model(self):
         model = Sequential()
@@ -40,3 +104,10 @@ class DqnAgent:
                       optimizer=Adam(lr=0.001),
                       metrics=["accuracy"])
         return model
+
+    def update_replay_memory(self, transition):
+        self.replay_memory.append(transition)
+
+    def get_Qs(self, state, step):
+        return self.model_predict(
+            np.array(state).reshape(-1, *state.shape) / 255)[0]
